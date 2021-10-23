199e59c64b90c0fa822462c1980e05e7a21d6b45
unknown <priyareddyece97@gmail.com>
2021-10-23 08:57:56 +0530

    first commit

--- Services/displayDetails.service.js
+++ Services/displayDetails.service.js
@@ -3,44 +3,17 @@ const { ObjectId } = require("mongodb");
 
 const service = {
     async displayDetails(req, res) {
-        const mentor = await mongo.db.collection("student").find({}, {
+        const mentor = await mongo.db.collection("student").find({mentorId : { $ne: 0 }}, {
             projection: {
-                _id: 1
+                _id: 0,
+                studentname:1,
+                mentorId:1
             }
         }).toArray();
         console.log(JSON.stringify(mentor));
+        res.send(mentor);
 
-        const details = await mongo.db.collection("student").aggregate([
-            {
-                $lookup:
-                {
-                    from: "mentor",
-                    localField: "mentorId",
-                    foreignField: "fakeid",
-                    // JSON.stringify("_id")
-                    as: "allDetails"
-                }
-            }
-            ,
-            {
-                $project:
-                {
-                    _id: 0,
-                    studentname: 1,
-                    mentorname: 1
-                }
-            }
-        ]).toArray()
-
-        // const filteredData = await details.find({}, {
-        //     projection: {
-        //         _id: 0,
-        //         studentname: 1,
-        //         mentorname: 1
-        //     }
-        // })
-        console.log(details);
-        res.send(details);
+        
     }
 }
 
